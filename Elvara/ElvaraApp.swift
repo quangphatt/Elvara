71b32fb3c4c47317d1357886569f9702aa7e1087
quangphatt <phatquang2811@gmail.com>
2026-04-08 11:50:47 +0700

    Add Firebase config

--- Elvara/ElvaraApp.swift
+++ Elvara/ElvaraApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct ElvaraApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
