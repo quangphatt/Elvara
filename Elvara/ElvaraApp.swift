1b6bff99dba699afcf730be7edb769b557a25395
quangphatt <phatquang2811@gmail.com>
2026-04-08 10:56:09 +0700

    Initial Commit

---
+++ Elvara/ElvaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvaraApp.swift
+//  Elvara
+//
+//  Created by Quang Phat on 8/4/26.
+//
+
+import SwiftUI
+
+@main
+struct ElvaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
